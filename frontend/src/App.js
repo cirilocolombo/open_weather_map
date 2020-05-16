f420f7327e87585891da44acfabdaed0596c67a7
Cirilo Colombo <cirilo.colombo@gmail.com>
2020-05-15 21:35:12 -0300

    Incluído a data na visualização

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
+require('dotenv/config');
  
 function App() {
 	const [location, setLocation] = useState(false);
@@ -11,14 +12,22 @@ function App() {
 			params: {
 				lat: lat,
 				lon: long,
-				appid: 'f7c4de0476923bb99ff956d686c021ac',
+				appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
 				lang: 'pt',
 				units: 'metric'
 			}  
 		});
 
 		setWeather(res.data);
-		console.log(process.env.REACT_APP_OPEN_WHEATHER_KEY); // alterar o appid quando conseguir fazer isto funcionar
+	}
+
+	let convertDateWeather = (date) => {
+		const unixTimestamp = date
+		const milliseconds = unixTimestamp * 1000
+		const dateObject = new Date(milliseconds)
+		const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+
+		return humanDateFormat;
 	}
 
   useEffect(()=> {
@@ -28,13 +37,13 @@ function App() {
     })
   }, []) // Quando este segundo argumento é vazio então executa o efeito somente no load da página
 
-	if (location == false) {
+	if (location === false) {
     return (
 			<Fragment>
       	Você precisa habilitar a localização no browser
 			</Fragment>
 		);
-	}	else if (weather == false) {
+	}	else if (weather === false) {
 		return (
 			<Fragment>
 				Carregando o clima...
@@ -46,6 +55,7 @@ function App() {
 				<h3>Clima em {weather['name']} agora ({weather['weather'][0]['description']})</h3>
 				<hr/>
 				<ul>
+					<li>Data: {convertDateWeather(weather['dt'])}</li>
 					<li>Temperatura atual: {weather['main']['temp']}°</li>
 					<li>Temperatura máxima: {weather['main']['temp_max']}°</li>
 					<li>Temperatura minima: {weather['main']['temp_min']}°</li>
